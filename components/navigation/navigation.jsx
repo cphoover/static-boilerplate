dfed98756573f480256fce54b139c4f02598a9ea
Charlie Hoover <choover@underarmour.com>
2016-03-25 22:40:43 -0400

    saving

---
+++ components/navigation/navigation.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export default class About extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+		};
+	}
+	handleChange(event) {
+		this.setState({
+			message: event.target.value
+		});
+	}
+	render() {
+		return <div>
+			<a href="/">Explore Projects</a>
+			<a href="/about">About</a>
+			<a href="//github.com/cphoover/project-explorer">Fork this project!</a>
+		</div>;
+	}
+}
+/*
+var React = require('react');
+var ReactDOM = require('react-dom');
+ReactDOM.render(React.createElement(About), document.body);
+*/
